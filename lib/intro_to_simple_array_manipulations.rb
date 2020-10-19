d322ac30c9b5d5b0e75596e5f697276c88a7b136
Victor Cordero <victordcordero@gmail.com>
2020-10-19 23:20:04 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+def using_push(array, string)
+array.push(string)
+end
+
+def using_unshift(array,string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+array.pop[6]
+deleted_string = "Antarctica"
+end
+
+def pop_with_args(array)
+array.pop[2]
+array.pop[3]
+deleted_string = "Chihuahua", "Shiba Inu"
+end
+
+def using_shift(array)
+  array.shift[0]
+  deleted_string = "Lagos"
+end
+
+def shift_with_args(array)
+  array.shift[2]
+  deleted_string = "Blue Bell Creameries", "Ben & Jerry's"
+end
